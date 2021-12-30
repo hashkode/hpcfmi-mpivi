0d5d07f9191b34ab14a2c16b5d753a59ce06c630
Tobias Klama <tobias.klama@tum.de>
2021-12-30 14:08:19 +0100

    Comparing md5sums and copying if compromised works.\n Todo: implement paths as variable at begin of test.sh file. Also delete checksum files.

--- utl/test.sh
+++ utl/test.sh
@@ -3,47 +3,52 @@
 # compare md5 check sums from /home/<user> dir and /var/temp/<user>
 # copy missing/wrong files
 
-if [ -d "/var/temp/$(whoami)" ]
+if [ -d "/var/tmp/$(whoami)" ]
 then
   md5sum /home/$(whoami)/data/data_debug/* > checksum_debug_home.chk
-  md5sum /var/temp/$(whoami)/data/data_debug/* > checksum_debug_temp.chk
+  md5sum /var/tmp/$(whoami)/data/data_debug/* > checksum_debug_temp.chk
   
-  if [md5sum --status -c checksum_debug_home.chk checksum_debug_temp.chk]
+  if md5sum --status -c checksum_debug_home.chk checksum_debug_temp.chk
   then
+    echo "dataset debug is okay"
   else
     echo "dataset data_debug is compromised --> copying from home"
-    cp -R /home/$(whoami)/data_debug/* /var/temp/$(whoami)/data/data_debug
+    cp -R /home/$(whoami)/data/data_debug/* /var/tmp/$(whoami)/data/data_debug
   fi
   
   md5sum /home/$(whoami)/data/data_small/* > checksum_small_home.chk
-  md5sum /var/temp/$(whoami)/data/data_small/* > checksum_small_temp.chk
+  md5sum /var/tmp/$(whoami)/data/data_small/* > checksum_small_temp.chk
   
-  if [md5sum --status -c checksum_small_home.chk checksum_small_temp.chk]
+  if md5sum --status -c checksum_small_home.chk checksum_small_temp.chk
   then
+    echo "dataset small is okay"
   else
     echo "dataset data_small is compromised --> copying from home"
-    cp -R /home/$(whoami)/data_debug/* /var/temp/$(whoami)/data/data_debug
+    cp -R /home/$(whoami)/data/data_debug/* /var/tmp/$(whoami)/data/data_debug
   fi
   
   md5sum /home/$(whoami)/data/data_normal/* > checksum_normal_home.chk
-  md5sum /var/temp/$(whoami)/data/data_normal/* > checksum_normal_temp.chk
+  md5sum /var/tmp/$(whoami)/data/data_normal/* > checksum_normal_temp.chk
   
-  if [md5sum --status -c checksum_normal_home.chk checksum_normal_temp.chk]
+  if md5sum --status -c checksum_normal_home.chk checksum_normal_temp.chk
   then
+    echo "dataset normal is okay"
   else
     echo "dataset data_normal is compromised --> copying from home"
-    cp -R /home/$(whoami)/data_debug/* /var/temp/$(whoami)/data/data_debug
+    cp -R /home/$(whoami)/data/data_debug/* /var/tmp/$(whoami)/data/data_debug
   fi
   
 else
   echo "no dataset in /var/temp/ --> copying files ..."
-  mkdir /var/temp/$(whoami)/data
-  cp -R /home/$(whoami)/data/* /var/temp/$(whoami)/data
+  mkdir /var/tmp/$(whoami)
+  mkdir /var/tmp/$(whoami)/data
+  cp -R /home/$(whoami)/data/* /var/tmp/$(whoami)/data
+  echo "copying finished"
 fi
 
 cd "$(dirname "$0")"
 
-cd ../build
-./mpi-vi
+#cd ../build
+#./mpi-vi
 
 # me="$(whoami)" for username
