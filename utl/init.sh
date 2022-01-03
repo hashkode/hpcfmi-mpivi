536bce42bf3d75eff7541c22f238b06b38789256
Tobias Krug <tobias.krug@tum.de>
2022-01-03 21:23:41 +0100

    Add automatic download from hpcfmi Strato HiDrive user

--- utl/init.sh
+++ utl/init.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-# todo download data set into home directory 
+cd "$(dirname "$0")"
+echo "> running init routine"
 
 # prepare files 
-home_path="/home/$(whoami)/data/"
-#var_path="/var/tmp/$(whoami)/data"
+home_path="/home/$(whoami)/data"
 
-cd "$(dirname "$0")"
+if [ -d "$home_path" ]
+then
+  echo ">> data directory already created" 
+else
+  mkdir $home_path
+fi
+
+scp -i sshkey hpcfmi@scp.hidrive.strato.com:hpcfmi/hpcmi-data.tar.gz $home_path
+
+tar -xvf $home_path/hpcmi-data.tar.gz -C $home_path
+mv $home_path/hpcmi-data/data_debug $home_path/data_debug
+mv $home_path/hpcmi-data/data_small $home_path/data_small
+mv $home_path/hpcmi-data/data_normal $home_path/data_normal
+rm -r $home_path/hpcmi-data
 
 if [ -d "$home_path/data_debug" ] 
 then
