08df3c37e8b6bac11d493185fef6fb9ac8caba86
Tobias Krug <tobias.krug@tum.de>
2022-01-22 17:17:23 +0100

    Fix Raspberry Pi target test script Exclude data set tarball from rsync

--- utl/rpi/prepareRpi.sh
+++ utl/rpi/prepareRpi.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "$0")"
 _root=$1
 _node1=$2
-_node2=$2
-_node3=$3
+_node2=$3
+_node3=$4
 
 cd ../
 make _prepareTarget host=${_root} runtype=init & make _prepareTarget host=${_node1} runtype=init & make _prepareTarget host=${_node2} runtype=init & make _prepareTarget host=${_node3} runtype=init
