e6f3d4334512c9d7f44655d165b987a2b585af48
Tobias Krug <tobias.krug@tum.de>
2022-01-22 06:42:53 +0100

    Remove unnecessary file copy operation

--- utl/init.sh
+++ utl/init.sh
@@ -5,27 +5,26 @@ cd "$(dirname "$0")"
 echo "> running init routine"
 
 # local paths
-home_path="/home/$(whoami)/data/"
 var_path="/var/tmp/$(whoami)/data/"
 
 # prepare data set directory
-if [ -d "$home_path" ]
+if [ -d "$var_path" ]
 then
   echo ">> data directory already created" 
-  if [ -d "$home_path/data_debug" ]
+  if [ -d "$var_path/data_debug" ]
   then
-    rm -rf "$home_path/data_debug" ]
+    rm -rf "$var_path/data_debug" ]
   fi
-  if [ -d "$home_path/data_small" ]
+  if [ -d "$var_path/data_small" ]
   then
-    rm -rf "$home_path/data_small"
+    rm -rf "$var_path/data_small"
   fi
-  if [ -d "$home_path/data_normal" ]
+  if [ -d "$var_path/data_normal" ]
   then
-    rm -rf "$home_path/data_normal"
+    rm -rf "$var_path/data_normal"
   fi
 else
-  mkdir $home_path
+  mkdir $var_path
 fi
 
 # permission adjustment for sshkey
@@ -33,41 +32,36 @@ chmod 400 sshkey
 
 # download data
 echo ">> fetch data set"
-rsync -e "ssh -i sshkey -o 'StrictHostKeyChecking no'" hpcfmi@rsync.hidrive.strato.com:hpcfmi/hpcmi-data.tar.gz $home_path -azrc -q
+rsync -e "ssh -i sshkey -o 'StrictHostKeyChecking no'" hpcfmi@rsync.hidrive.strato.com:hpcfmi/hpcmi-data.tar.gz $var_path -azrc -q
 
 # extract data
-tar -xvf $home_path/hpcmi-data.tar.gz -C $home_path >/dev/null
-mv $home_path/hpcmi-data/data_debug $home_path
-mv $home_path/hpcmi-data/data_small $home_path
-mv $home_path/hpcmi-data/data_normal $home_path
-rm -r $home_path/hpcmi-data
+tar -xvf $var_path/hpcmi-data.tar.gz -C $var_path >/dev/null
+mv $var_path/hpcmi-data/data_debug $var_path
+mv $var_path/hpcmi-data/data_small $var_path
+mv $var_path/hpcmi-data/data_normal $var_path
+rm -r $var_path/hpcmi-data
 
 # prepare data, i.e. convert parameters in txt-format for c++ handling
-if [ -d "$home_path/data_debug" ] 
+if [ -d "$var_path/data_debug" ]
 then
-  python3 preparedata.py -d $home_path/data_debug
+  python3 preparedata.py -d $var_path/data_debug
   echo "debug dataset prepared"
 else
-  echo "no debug dataset available in $home_path"
+  echo "no debug dataset available in $var_path"
 fi
 
-if [ -d "$home_path/data_small" ] 
+if [ -d "$var_path/data_small" ]
 then
-  python3 preparedata.py -d $home_path/data_small
+  python3 preparedata.py -d $var_path/data_small
   echo "small dataset prepared"
 else
-  echo "no small dataset available in $home_path"
+  echo "no small dataset available in $var_path"
 fi
 
-if [ -d "$home_path/data_normal" ] 
+if [ -d "$var_path/data_normal" ]
 then
-  python3 preparedata.py -d $home_path/data_normal
+  python3 preparedata.py -d $var_path/data_normal
   echo "normal dataset prepared"
 else
-  echo "no normal dataset available in $home_path"
+  echo "no normal dataset available in $var_path"
 fi
-
-### sync data set from home drive to tmp
-echo ">> sync data set to /var/tmp/"
-mkdir -p ${var_path}
-rsync -azrq ${home_path} ${var_path} --delete --exclude=hpcmi-data.tar.gz
